0aa1b5b38ced927de63f8525c7bec3f84540c904
regisls <regisls@gmail.com>
2022-10-03 12:09:18 -0300

    Rename sqlcmd.bat to SQL_Exec_SqlCmd.bat

---
+++ cmd/SQL_Exec_SqlCmd.bat
@@ -0,0 +1,2 @@
+sqlcmd -S localhost\sqlexpress -E -i c:\GitHub\SQLUteis\SQL\02-create-database.sql
+sqlcmd -S localhost\sqlexpress -E -i c:\GitHub\SQLUteis\SQL\03-create-login-user.sql
--- cmd/sqlcmd.bat
+++
@@ -1,2 +0,0 @@
-sqlcmd -S localhost\sqlexpress -E -i c:\GitHub\SQLUteis\SQL\02-create-database.sql
-sqlcmd -S localhost\sqlexpress -E -i c:\GitHub\SQLUteis\SQL\03-create-login-user.sql
